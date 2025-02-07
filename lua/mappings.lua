1b63c02b7258ad7f564c6e3fce38f9d1c387d31a
Anton-beep <anton.plyu@gmail.com>
2025-02-07 23:07:44 +0100

    tree refresh on focus

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,9 +9,17 @@ map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
-map('i', '<C-l>', function ()
-  vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
-end, { desc = 'Copilot Accept', noremap = true, silent = true })
+map("i", "<C-l>", function()
+  vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+end, { desc = "Copilot Accept", noremap = true, silent = true })
 
-map('n', 'cc', ":CopilotChatToggle<CR>", { desc = 'Copilot Chat Toggle', noremap = true, silent = true })
+map("n", "cc", ":CopilotChatToggle<CR>", { desc = "Copilot Chat Toggle", noremap = true, silent = true })
 
+map("n", "sh", ":split<CR>", { desc = "Split Horizontal", noremap = true, silent = true })
+map("n", "sv", ":vsplit<CR>", { desc = "Split Vertical", noremap = true, silent = true })
+
+-- refresh tree when focus
+map("n", "<leader>e", function()
+  vim.cmd "NvimTreeFocus"
+  vim.cmd "NvimTreeRefresh"
+end, { desc = "nvimtree focus window" })
