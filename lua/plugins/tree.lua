6de706c6fa2648eb225b5e7ba48b821e34963a80
Anton-beep <anton.plyu@gmail.com>
2025-02-08 00:03:50 +0100

    nvim tree dotenv files are visible now

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -2,16 +2,7 @@ return {
   "nvim-tree/nvim-tree.lua",
   cmd = { "NvimTreeToggle", "NvimTreeFocus" },
   opts = function()
-    return require "nvchad.configs.nvimtree"
+    return require "configs.nvimtree"
   end,
-  config = function()
-    require "nvim-tree".setup({
-      filters = {
-        dotfiles = false
-      },
-      view = {
-        width = 40
-      }
-    })
-  end
+  lazy = false
 }
