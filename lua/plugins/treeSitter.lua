3bcd8cce7818235590d8e55f1c3a1b91c97b7cda
Anton-beep <anton.plyu@gmail.com>
2025-02-07 23:39:35 +0100

    better higlight and rainbow brackets

--- lua/plugins/treeSitter.lua
+++ lua/plugins/treeSitter.lua
@@ -1,4 +1,19 @@
-  return {
-    "nvim-treesitter/nvim-treesitter",
-    auto_install = true
-  }
+return {
+  "nvim-treesitter/nvim-treesitter",
+  auto_install = true,
+  config = function()
+    pcall(function()
+      dofile(vim.g.base46_cache .. "syntax")
+      dofile(vim.g.base46_cache .. "treesitter")
+    end)
+
+    require("nvim-treesitter.configs").setup {
+      ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc" },
+      highlight = {
+        enable = true,
+        use_languagetree = true,
+      },
+      indent = { enable = true },
+    }
+  end,
+}
