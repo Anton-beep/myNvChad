5251d3eefd6c1147319235a8e1f664558baae7f2
Anton-beep <anton.plyu@gmail.com>
2025-05-19 10:30:56 +0200

    added lsp for bash and c/cpp, changed deprecated feature from neovide

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "ts_ls", "harper_ls", "docker_compose_language_service", "dockerls"}
+local servers = { "html", "cssls", "ts_ls", "harper_ls", "docker_compose_language_service", "dockerls", "clangd", "bashls"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
