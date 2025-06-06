f93ee7fd545437894bb38bb4e9dea4f366f33389
Anton-beep <anton.plyu@gmail.com>
2025-06-06 18:58:51 +0200

    added golang lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "ts_ls", "harper_ls", "docker_compose_language_service", "dockerls", "clangd", "bashls", "ruff"}
+local servers = { "html", "cssls", "ts_ls", "harper_ls", "docker_compose_language_service", "dockerls", "clangd", "bashls", "ruff", "gopls", "golangci_lint_ls"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
