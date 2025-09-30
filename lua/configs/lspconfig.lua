cdba6a901980eaada3b63e6be71410f2870c76f4
Anton-beep <anton.plyu@gmail.com>
2025-09-30 23:07:35 +0200

    fixed deprecated function

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,35 +1,16 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
 local servers = require "configs.lspServers"
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  vim.lsp.config(lsp, {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
-  }
+  })
 end
 
 -- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
---
-
-lspconfig.harper_ls.setup {
-  settings = {
-    ["harper-ls"] = {
-      linters = {
-        SentenceCapitalization = false,
-        SpellCheck = false
-      }
-    }
-  }
-}
