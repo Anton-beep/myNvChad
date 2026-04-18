b1378a838a73ea21ef5ae3f76b2ef6446a121cc4
Anton-beep <anton.plyu@gmail.com>
2026-04-18 18:32:46 +0200

    removed treesitter refactor

--- lua/plugins/treeSitter.lua
+++ lua/plugins/treeSitter.lua
@@ -1,20 +1,21 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   auto_install = true,
   config = function()
-    pcall(function()
-      dofile(vim.g.base46_cache .. "syntax")
-      dofile(vim.g.base46_cache .. "treesitter")
-    end)
+    -- pcall(function()
+    --   dofile(vim.g.base46_cache .. "syntax")
+    --   dofile(vim.g.base46_cache .. "treesitter")
+    -- end)
 
-    require("nvim-treesitter.configs").setup {
-      ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc" },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        use_languagetree = true,
-      },
-      indent = { enable = true },
-    }
+    -- require("nvim-treesitter.configs").setup {
+    --   ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc" },
+    --   auto_install = true,
+    --   highlight = {
+    --     enable = true,
+    --     use_languagetree = true,
+    --   },
+    --   indent = { enable = true },
+    -- }
   end,
 }
