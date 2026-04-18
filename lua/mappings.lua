a6005276cefd8e49b6a548e03084b810e0b533ed
Anton-beep <anton.plyu@gmail.com>
2026-04-18 18:07:03 +0200

    Added opencode, removed copilotchat

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,8 +13,6 @@ map("i", "<C-l>", function()
   vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
 end, { desc = "Copilot Accept", noremap = true, silent = true })
 
-map("n", "<leader>cc", ":CopilotChatToggle<CR>", { desc = "Copilot Chat Toggle", noremap = true, silent = true })
-
 map("n", "sh", ":split<CR>", { desc = "Split Horizontal", noremap = true, silent = true })
 map("n", "sv", ":vsplit<CR>", { desc = "Split Vertical", noremap = true, silent = true })
 
@@ -23,3 +21,13 @@ map("n", "<leader>e", function()
   vim.cmd "NvimTreeFocus"
   vim.cmd "NvimTreeRefresh"
 end, { desc = "nvimtree focus window" })
+
+map({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end,
+  { desc = "Ask opencode…" })
+map({ "n", "x" }, "<leader>ox", function() require("opencode").select() end, { desc = "Execute opencode action…" })
+map({ "n", "t" }, "<leader>ot", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
+
+map("n", "<leader>ou", function() require("opencode").command("session.half.page.up") end, { desc = "Scroll opencode up" })
+map("n", "<leader>od", function() require("opencode").command("session.half.page.down") end,
+  { desc = "Scroll opencode down" })
+
